ea09c4fee1c5402d68ea96a2e387844135d1d5dd
Anshul Patel <anshulpatel5757@gmail.com>
2023-08-21 23:39:21 +0530

    login api created with validation of pass in backed , rejecting with particular errors and feedback

--- controller/Auth.js
+++ controller/Auth.js
@@ -12,3 +12,27 @@ exports.signupUserAccount = async (req, res) => {
     // console.log(err, "Not OKKKKKKKKKKK");
   }
 };
+
+//login User
+exports.loginUserAccount = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email }).exec();
+    if (!user) {
+      res.status(404).json({ message: "• User Not found !" });
+    } else if (req.body.password === user.password) {
+      ///TODO: this woud be encrypted
+      res.status(201).json({
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        addresses: user.addresses,
+        role: user.role,
+      });
+    } else {
+      res.status(401).json({ message: "• Invalid Credentials !" });
+    }
+  } catch (err) {
+    res.status(400).json(err);
+    // console.log(err, "Not OKKKKKKKKKKK");
+  }
+};
--- routes/Auth.js
+++ routes/Auth.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { signupUserAccount } = require("../controller/Auth");
+const { signupUserAccount, loginUserAccount } = require("../controller/Auth");
 
 const router = express.Router();
 
-router.post("/signup", signupUserAccount);
+router.post("/signup", signupUserAccount).post("/login", loginUserAccount);
 
 exports.router = router;
